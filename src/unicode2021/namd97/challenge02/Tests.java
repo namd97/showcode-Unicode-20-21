c9a73a91a35106760c8c5a27b991befe8328f0df
Duncan Nam <duncannam@gmail.com>
2021-05-21 15:57:57 +0100

    challenge2 hiddentests added

--- src/unicode2021/namd97/challenge02/Tests.java
+++ src/unicode2021/namd97/challenge02/Tests.java
@@ -22,4 +22,74 @@ public class Tests {
 
 		assertEquals(0, solution.devvie("FxLxLxFx"));
 	}
+	
+	/**
+	 * 1 Pts | Check that FFF returns 5.
+	 */
+	@Test
+	public void hiddentest1() {
+		Solution solution = new Solution();
+
+		assertEquals(5, solution.devvie("FFF"));
+	}
+	
+	/**
+	 * 1 Pts | Check that RRR returns 0.
+	 */
+	@Test
+	public void hiddentest2() {
+		Solution solution = new Solution();
+
+		assertEquals(0, solution.devvie("RRR"));
+	}
+	
+	/**
+	 * 3 Pts | Check that FFLFRF returns 6.
+	 */
+	@Test
+	public void hiddentest3() {
+		Solution solution = new Solution();
+
+		assertEquals(6, solution.devvie("FFLFRF"));
+	}
+	
+	/**
+	 * 3 Pts | Check that FFFLF returns 6.
+	 */
+	@Test
+	public void hiddentest4() {
+		Solution solution = new Solution();
+
+		assertEquals(6, solution.devvie("FFFLF"));
+	}
+	
+	/**
+	 * 3 Pts | Check that LFFRFRFFFF returns 5.
+	 */
+	@Test
+	public void hiddentest5() {
+		Solution solution = new Solution();
+
+		assertEquals(5, solution.devvie("LFFRFRFFFF"));
+	}
+	
+	/**
+	 * 2 Pts | Check that lower case letters are ignored.
+	 */
+	@Test
+	public void hiddentest6() {
+		Solution solution = new Solution();
+
+		assertEquals(0, solution.devvie("RRRrrrrrrrr"));
+	}
+	
+	/**
+	 * 2 Pts | Check that spaces and junk are ignored.
+	 */
+	@Test
+	public void hiddentest7() {
+		Solution solution = new Solution();
+
+		assertEquals(0, solution.devvie("FxLx   %&£(  )LxFx"));
+	}
 }
